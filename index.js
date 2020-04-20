fed38993d28e9b3ab43e114ca2a350476425bdb2
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-20 15:55:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,3 @@ array.foreach(callback(element,index,array));
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
-
